e09f0018fe7aa85d2883e990da0c5ce3871e5845
jeremias <jeremiaszr54@gmail.com>
2023-08-03 18:52:32 -0300

    Entrega final

--- js/main.js
+++ js/main.js
@@ -14,15 +14,24 @@ formu.addEventListener('submit', (e) => {
         enlace.href = 'pages/principal.html';
         enlace.innerText = 'Ir a otra página';
         boton.appendChild(enlace);
-
         let mensaje2 = document.getElementById('mensaje2');
         mensaje2.innerHTML = '';
+        Toastify({
+            text: "Inicio secion con exito",
+            className: "info",
+            style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+            }
+        }).showToast();
         mensaje2.appendChild(boton);
     } else {
         mensaje1.innerHTML = '';
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Debes de completar todo',
+            footer: 'Disculpe la molestia</p>'
+        })
         mensaje1.innerHTML="<strong>Debes ingresar tanto el usuario como el apellido.</strong>"
     }
 });
-
-
-
--- js/main2.js
+++ js/main2.js
@@ -1,115 +1,178 @@
-//pagina2 grande
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var objeto = {
-    nombre: nombre,
-    apellido: apellido,
-    };
-    var arrayObjetos = [];
-    arrayObjetos.push(objeto);
-    console.log(arrayObjetos);
-});
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var objeto = {
-        nombre: nombre,
-        apellido: apellido
-    };
-    var arrayObjetos = JSON.parse(localStorage.getItem('arrayObjetos')) || [];
-    arrayObjetos.push(objeto);
-    localStorage.setItem('arrayObjetos', JSON.stringify(arrayObjetos));
-    mostrarNombres(arrayObjetos);
-    mostrarApellidos(arrayObjetos);
-});
-function mostrarNombres(arrayObjetos) {
-    var nombres = arrayObjetos.map(function(obj) {
-        return obj.nombre;
-    });
-    document.getElementById('nombrescli').innerHTML = '<ol><li>' + nombres.join('</li><li>') + '</li></ol>';
-}
-function mostrarApellidos(arrayObjetos) {
-    var apellidos = arrayObjetos.map(function(obj) {
-        return obj.apellido;
-    });
-    document.getElementById('apellidoscli').innerHTML = '<ol><li>' + apellidos.join('</li><li>') + '</li></ol>';
-}
 document.addEventListener('DOMContentLoaded', function() {
-    var arrayObjetos = JSON.parse(localStorage.getItem('arrayObjetos')) || [];
-    mostrarNombres(arrayObjetos);
-    mostrarApellidos(arrayObjetos);
-});
-document.getElementById('eliminarDatos').addEventListener('click', function() {
-    localStorage.removeItem('arrayObjetos');
-    document.getElementById('nombrescli').innerHTML = '';
-    document.getElementById('apellidoscli').innerHTML = '';
-});
-let saludar=sessionStorage.getItem(`nombreUsuario`)
-let dentrosaludar=document.getElementById("Saludar")
-
-dentrosaludar.innerHTML=`<h1>Hola usuario ${saludar}</h1>`
-
-let contenedorEliminartodo = document.getElementById("busquedaa")
-
-let inputBusqueda = document.createElement('input');
-inputBusqueda.setAttribute('type', 'text');
-inputBusqueda.setAttribute('id', 'busqueda');
-inputBusqueda.setAttribute('placeholder', 'Buscar por apellido');
-contenedorEliminartodo.appendChild(inputBusqueda);
-
-let botonBuscar = document.createElement('button');
-botonBuscar.setAttribute('id', 'buscar');
-botonBuscar.innerText = 'Buscar';
-botonBuscar.className=`nada`
-contenedorEliminartodo.appendChild(botonBuscar);
-document.getElementById('buscar').addEventListener('click', function() {
-    let apellidoBuscado = document.getElementById('busqueda').value;
-    mostrarApellido(apellidoBuscado);
-});
-
-function mostrarApellido(apellido) {
-    let arrayObjetos = JSON.parse(localStorage.getItem('arrayObjetos')) || [];
-    let indice = -1;
-    for (let i = 0; i < arrayObjetos.length; i++) {
-        if (arrayObjetos[i].apellido === apellido) {
-            indice = i;
-            break;
+    let personas = [];
+    const personasJSON = localStorage.getItem('personas');
+    if (personasJSON) {
+        personas = JSON.parse(personasJSON);
+    }
+    function guardarPersonasEnLocalStorage(personas) {
+        localStorage.setItem('personas', JSON.stringify(personas));
+    }
+    async function guardarPersonasEnServidor(personas) {
+        try {
+            const response = await fetch('http://localhost:3000/user', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(personas)
+            });
+            const data = await response.json();
+        } catch (error) {
+            console.error('Error al enviar datos al servidor:', error);
         }
     }
-    if (indice !== -1) {
-        let apellidoEncontrado = arrayObjetos[indice].apellido;
-
-        var elementoApellido = document.createElement('p');
-        elementoApellido.innerText = apellidoEncontrado;
-        
-        let botonEliminar = document.createElement('button');
-        botonEliminar.innerText = 'Eliminar';
-        botonEliminar.className=`nada`
-        botonEliminar.addEventListener('click', function() {
-            arrayObjetos.splice(indice, 1);
-            localStorage.setItem('arrayObjetos', JSON.stringify(arrayObjetos));
-            mostrarNombres(arrayObjetos);
-            mostrarApellidos(arrayObjetos);
-            contenedorEliminartodo.removeChild(elementoApellido);
-            contenedorEliminartodo.removeChild(botonEliminar);
-        });
-        contenedorEliminartodo.appendChild(elementoApellido);
-        contenedorEliminartodo.appendChild(botonEliminar);
-    } else {
-        let noseencontro=document.getElementById("nada")
-        noseencontro.innerHTML=`<h3>No se encontro ningun apellido</h3>`
+    const nombreUsuario = sessionStorage.getItem("nombreUsuario");
+    //saludar
+    if (nombreUsuario) {
+        const saludarDiv = document.getElementById('saludar');
+        saludarDiv.textContent = `¡Hola, ${nombreUsuario}! Bienvenido (los registro de pacientes)`;
+        setTimeout(function() {
+            saludarDiv.textContent = '';
+        }, 30000);
     }
-}
-
-
-
-
-
-
-
-
-
-
+    document.getElementById('datosdeentrada').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const nombre = document.getElementById('nomPa').value;
+        const apellido = document.getElementById('apePa').value;
+        const dni = document.getElementById('dniPa').value;
+        const turno = document.getElementById('turnoPa').value;
+        if (!nombre || !apellido || !dni || !turno) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Debes de ingresar todos los datos!'
+            })
+            return;
+        }
+        const documentoExistente = personas.some(persona => persona.dni === dni);
+        if (documentoExistente) {
+            Swal.fire({
+                icon: 'error',
+                title: 'DNI iguales',
+                text: 'Debes de ingresar un DNI diferente'
+            })
+            return;
+        }
+        const fechaExistente = personas.some(persona => persona.turno === turno);
+        if (fechaExistente) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Turno ya ocupado',
+                text: 'Debes de ingresar un turno diferente'
+            })
+            return;
+        }
+        Toastify({
+            text: "Se guardo con exito",
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+                },
+            duration: 2000
+            }).showToast();
+        const persona = {
+            nombre: nombre,
+            apellido: apellido,
+            dni: dni,
+            turno: turno
+        };
+        personas.push(persona);
+        guardarPersonasEnLocalStorage(personas);
+        guardarPersonasEnServidor(personas);
+        document.getElementById('datosdeentrada').reset();
+        mostrarDatos(personas);
+    });
+    document.getElementById('busquedaForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const apellidoBuscar = document.getElementById('buscarApellido').value.trim();
+        if (!apellidoBuscar) {
+            Toastify({
+                text: 'Ingrese un apellido para buscar.',
+                style: {
+                    background: 'linear-gradient(to right, #b40909, #e95d0f)',
+                },
+                duration: 2000
+            }).showToast();
+            return;
+        }
+        const personaEncontrada = personas.find(persona => persona.apellido.toLowerCase() === apellidoBuscar.toLowerCase());
+        if (personaEncontrada) {
+            const nombreBus = document.getElementById('nombrebus');
+            const apellBus = document.getElementById('apellidobus');
+            const turnoBus = document.getElementById('turnobus');
+            nombreBus.textContent = 'Nombre: ' + personaEncontrada.nombre;
+            apellBus.textContent = 'Apellido: ' + personaEncontrada.apellido;
+            turnoBus.textContent = 'Fecha del turno: ' + personaEncontrada.turno;
+            const btnEliminar = document.getElementById('eliminarPaciente');
+            btnEliminar.style.display = 'block';
+            btnEliminar.addEventListener('click', function() {
+            });
+        } else {
+            Toastify({
+                text: 'Paciente no encontrado.',
+                style: {
+                    background: 'linear-gradient(to right, #b40909, #e95d0f)',
+                },
+                duration: 2000
+            }).showToast();
+        }
+    });
+    // Función para mostrar los datos guardados
+    function mostrarDatos(personas) {
+        const datosDiv = document.querySelector('.datos');
+        datosDiv.innerHTML = '';
+        personas.forEach(function(persona, index) {
+            const cardDiv = document.createElement('div');
+            cardDiv.classList.add('card');
+            const nombreSpan = document.createElement('span');
+            nombreSpan.textContent = persona.nombre + ' ' + persona.apellido;
+            const turnoP = document.createElement('p');
+            turnoP.classList.add('job');
+            turnoP.textContent = `DNI: ${persona.dni}`;
+            const eliminarButton = document.createElement('button');
+            eliminarButton.textContent = 'Eliminar';
+            eliminarButton.addEventListener('click', function() {
+                // Eliminar la persona del array y actualizar el almacenamiento local
+                const swalWithBootstrapButtons = Swal.mixin({
+                    customClass: {
+                        confirmButton: 'btn btn-success',
+                        cancelButton: 'btn btn-danger'
+                    },
+                    buttonsStyling: false
+                })
+                swalWithBootstrapButtons.fire({
+                    title: 'Esta seguro de elimar este turno?',
+                    text: "Una vez elimando no se podra recuperar",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Si, estoy seguro!',
+                    cancelButtonText: 'No, cancelar!',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        swalWithBootstrapButtons.fire(
+                        'Eliminado!',
+                        'El turno fue eliminado.',
+                        'success',
+                        personas.splice(index, 1),
+                        guardarPersonasEnLocalStorage(personas),
+                        mostrarDatos(personas)
+                    )
+                    } else if (
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        swalWithBootstrapButtons.fire(
+                        'Cancelado',
+                        'El turno no fue borrado :)',
+                        'error'
+                    )
+                    }
+                })
+                
+            });
+            cardDiv.appendChild(nombreSpan);
+            cardDiv.appendChild(turnoP);
+            cardDiv.appendChild(eliminarButton);
+            datosDiv.appendChild(cardDiv);
+        });
+    }
+    // Mostrar los datos iniciales al cargar la página
+    mostrarDatos(personas);
+});
